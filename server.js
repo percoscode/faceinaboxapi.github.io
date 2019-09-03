4621bea320e1888d563df15ee3a6d1f547549150
percoscode <50783968+percoscode@users.noreply.github.com>
2019-09-02 19:17:38 -0700

    added bcrypt, also profile :id, and image paths

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 app.use(bodyParser.json());
+
+const getUserId = () => {
+    // const { id } = req.params;
+    // let found = false;
+    // database.users.forEach(user => {
+    //     if (user.id === id) {
+    //         found = true;
+    //         return res.json(user);
+    //     }
+    // })
+    // if (!found) {
+    //     res.status(400).json('not found');
+    // }
+}
 
 const database = {
     users: [
@@ -23,6 +38,13 @@ const database = {
             joined: new Date()
         }
     ]
+    // login: [
+    //     {
+    //         id: '987',
+    //         hash: '',
+    //         email: 'john@aasdgiajrge.com'
+    //     }
+    // ]
 }
 
 app.get('/', (req, res) => {
@@ -30,6 +52,17 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
+    // Load hash from your password DB.
+    bcrypt.compare("apples", hash, function(err, res) {
+        //this wont work unless you use dummy hash
+        console.log('first guess', res)
+        res === true
+    });
+    bcrypt.compare("not_bacon", hash, function(err, res) {
+        //this wont work unless you use dummy hash
+        console.log('second guess')
+        res === false
+    });
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
         res.json('success')
@@ -41,6 +74,12 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash("B4c0/\/", salt, function(err, hash) {
+            // Store hash in your password DB.
+            console.log(hash)
+        });
+    });
     database.users.push({
         id: '123',
         name: name,
@@ -51,6 +90,50 @@ app.post('/register', (req, res) => {
     })
     res.json(database.users[database.users.length-1])
 })
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+    // getUserId()
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+    // getUserId()
+})
+
+// Load hash from your password DB.
+// bcrypt.compare("B4c0/\/", hash, function(err, res) {
+//     // res === true
+// });
+// bcrypt.compare("not_bacon", hash, function(err, res) {
+//     // res === false
+// });
+ 
+// // As of bcryptjs 2.4.0, compare returns a promise if callback is omitted:
+// bcrypt.compare("B4c0/\/", hash).then((res) => {
+//     // res === true
+// });
 
 app.listen(3000, ()=> {
     console.log('app is running on port 3000');
